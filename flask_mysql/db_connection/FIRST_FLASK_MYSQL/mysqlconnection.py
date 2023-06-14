02f6c0cc30c7fe8ffff180df86602f628bdd2f21
Yancenteno <yancie.centeno@gmail.com>
2023-06-14 12:07:18 -0400

    First Commit

--- flask_mysql/db_connection/FIRST_FLASK_MYSQL/mysqlconnection.py
+++ flask_mysql/db_connection/FIRST_FLASK_MYSQL/mysqlconnection.py
@@ -39,4 +39,4 @@ class MySQLConnection:
 # this connectToMySQL function creates an instance of MySQLConnection, which will be used by server.py
 # connectToMySQL receives the database we're using and uses it to create an instance of MySQLConnection
 def connectToMySQL(db):
-    return MySQLConnection(db)
+    return MySQLConnection(db) 
